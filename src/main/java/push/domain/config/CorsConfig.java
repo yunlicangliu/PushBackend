8ef7e41380ca22eec66e2735d854a5c05ba6d789
yunlicangliu <18302010023@fudan.edu.cn>
2021-04-29 20:06:19 +0800

    first commit

---
+++ src/main/java/push/domain/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package push.domain.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .allowedOrigins("*")
+                .maxAge(3600);
+    }
+}
