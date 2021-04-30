bf8e7df5baee209597f3fdbc2a44bd6638196148
Yu Zhexuan <280866429@qq.com>
2021-04-30 11:59:46 +0800

    debug

--- src/main/java/fudan/push/controller/HelloController.java
+++ src/main/java/fudan/push/controller/HelloController.java
@@ -5,9 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
+@RestController
 public class HelloController {
 
 
